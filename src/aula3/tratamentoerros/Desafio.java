9df1bcbab6d701025e7ece2fe9711df957deb28f
Henrique Castanheiro <henrique.castanheirotb@gmail.com>
2024-08-01 14:35:00 -0300

    Correções Aula03

--- src/aula3/tratamentoerros/Desafio.java
+++ src/aula3/tratamentoerros/Desafio.java
@@ -11,36 +11,43 @@ public class Desafio {
 
 		try {
 			Venda venda = new Venda();
+
 			venda.setNomeCliente(JOptionPane.showInputDialog(
 					"Informe o nome do cliente: "));
 			int qtddItens = Integer.parseInt(JOptionPane.showInputDialog(
 					"Informe a quantidade de itens:"));
+			// Verifica se a quantidade de itens é válida entre 1 e 10
 			if (qtddItens <= 0 || qtddItens > 10) {
 				throw new VendaException(null);
 			}
 
 			for (int i = 0; i < qtddItens; i++) {
 				Item item = new Item();
-				item.nome = JOptionPane.showInputDialog(
-						"Informe o nome do " + (i + 1) + "º item: ");
+				item.nome = JOptionPane.showInputDialog("Informe o nome do " + (i + 1) + "º item: ");
+
+				// Loop validar o preço do item
 				do {
 					try {
-						item.preco = Double.parseDouble(JOptionPane.showInputDialog(
-										"Informe o valor de " + (item.nome) + ": "));
+						item.preco = Double
+								.parseDouble(JOptionPane.showInputDialog("Informe o valor de " + (item.nome) + ": "));
+						// Verifica se o preço é válido, maior que 0
 						if (item.preco <= 0) {
 							throw new PrecoIncorretoException(null);
 						}
 					} catch (PrecoIncorretoException e) {
+						// Exibe mensagem de erro se o preço for inválido
 						JOptionPane.showMessageDialog(null, e.getMessage());
 					}
 				} while (item.preco <= 0);
 
 				venda.itens.add(item);
 			}
+
 			venda.setItens(venda.itens);
+			// Gera e exibe o cupom fiscal
 			JOptionPane.showMessageDialog(null, venda.geraCupomFiscal());
 		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, e.getMessage());
+			JOptionPane.showMessageDialog(null, "Erro 05: Entre em contato com henrique_castanheirotb@gmail.com");
 		}
 	}
 }
--- src/aula3/tratamentoerros/Venda.java
+++ src/aula3/tratamentoerros/Venda.java
@@ -3,36 +3,43 @@ package aula3.tratamentoerros;
 import java.util.ArrayList;
 
 public class Venda {
-	private String nomeCliente;
-	ArrayList<Item> itens;
-	
-	public String getNomeCliente() {
-		return nomeCliente;
-	}
-	public void setNomeCliente(String nomeCliente) {
-		this.nomeCliente = nomeCliente;
-	}
-	public ArrayList<Item> getItens() {
-		return itens;
-	}
-	public void setItens(ArrayList<Item> itens) {
-		this.itens = itens;
-	}
-	
-	public String geraCupomFiscal() {
-		String txt = "      CUPOM FISCAL \n\n"
-			+ "Nome do cliente: " + this.nomeCliente
-			+ "  \nQuantidade de itens: " + this.itens.size()
-			+ "  \n                \nProdutos comprados: \n";
-		
-		double total = 0;
-			for (Item i : itens) {
-				txt += "\t-> " + i.getNome() + "\nPreço: R$ " + i.getPreco() + "\n\n";
-				total += i.getPreco();
-			}			
-			
-			txt += "===TOTAL: R$ "+ total + "===" + "\n\n------ VOLTE SEMPRE! ------";
-			
-		return txt;
-	}
+    private String nomeCliente;
+    ArrayList<Item> itens;
+
+    public Venda() {
+        this.itens = new ArrayList<>();
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public ArrayList<Item> getItens() {
+        return itens;
+    }
+
+    public void setItens(ArrayList<Item> itens) {
+        this.itens = itens;
+    }
+
+    public String geraCupomFiscal() {
+        String txt = "          CUPOM FISCAL \n\n"
+            + "Nome do cliente: " + this.nomeCliente
+            + "  \nQuantidade de itens: " + this.itens.size()
+            + "\n\n     Produtos Comprados \n";
+
+        double total = 0;
+        for (Item i : itens) {
+            txt += "\nItem: " + i.getNome() + "\nPreço: R$ " + i.getPreco() + "\n";
+            total += i.getPreco();
+        }
+
+        txt += "\nTotal: R$ " + total;
+
+        return txt;
+    }
 }
