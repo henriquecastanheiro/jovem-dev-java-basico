75970c016b6688b5704c681d3eb15d129febbea0
henriquecastanheiro <henrique.castanheirotb@gmail.com>
2024-08-02 11:59:19 -0300

    Prova não concluida

--- src/avaliacao/semana1/Principal.java
+++ src/avaliacao/semana1/Principal.java
@@ -3,9 +3,17 @@ package avaliacao.semana1;
 public class Principal {
 
 public static void main(String[] args) {
-	 
+	/*
+	 *       PROVA NÃO CONCLUIDA
+	 * 
+	 * 1 - Não foi gerado cupom;
+	 * 2 - Implementação do Clinte Premium;
+	 * 3 - Implementação do desconto;
+	 * 4 - Implementação do refence;
+	 * 5 - Implementação Lambda;
+	 */
+	
 	Venda.informacaoCliente();
 	ItemVenda.comprasProduto();
-		
 }
 }
